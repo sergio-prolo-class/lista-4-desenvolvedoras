af107e1e73b461e6b2cdf4fba9e775b4e3efb68d
fenamattos <94716415+fenamattos@users.noreply.github.com>
2025-07-06 22:28:58 -0300

    Update NaveCargueira.java

--- app/src/main/java/ifsc/poo/NaveCargueira.java
+++ app/src/main/java/ifsc/poo/NaveCargueira.java
@@ -1,51 +1,40 @@
 package ifsc.poo;
 
 public class NaveCargueira extends NaveEspacial implements Blindada, Tripulada {
+    private int capacidadeMaximaCarga = 100;
+    private int cargaAtual = 0;
+    private boolean blindagemAtiva = false;
 
-    private int capacidadeMaximaCarga;
-    private int cargaAtual;
+    public NaveCargueira() { super(14, 5); }
 
-    public NaveCargueira() {
-        super(14, 5);
-        this.capacidadeMaximaCarga = 100; // Valor de exemplo para a capacidade
-        this.cargaAtual = 0;
-    }
-
-    // Método obrigatório da interface Blindada
-    @Override
     public String ativarBlindagem() {
-        return "NaveCargueira (ID#" + this.id + "): blindagem ativada.";
+        blindagemAtiva = true;
+        return "NaveCargueira (ID#" + id + "): blindagem ativada.";
     }
 
-    // Método obrigatório da interface Tripulada
-    @Override
-    public String controlarManual() {
-        return "NaveCargueira (ID#" + this.id + "): controle manual ativado.";
-    }
-
-    // Método específico da NaveCargueira
-    public String carregar(int peso) {
-        this.cargaAtual = this.cargaAtual + peso;
-        String relatorioCarga = "NaveCargueira (ID#" + this.id + "): Carga atual é " + this.cargaAtual;
-
-        if ((double) this.cargaAtual / this.capacidadeMaximaCarga > 0.7) {
-            // Relaciona a mensagem de carga com a mensagem de ativação da blindagem.
-            relatorioCarga = relatorioCarga + ". " + this.ativarBlindagem();
+    public String desativarBlindagem() {
+        if (velocidadeAtual == 0) {
+            blindagemAtiva = false;
+            return "NaveCargueira (ID#" + id + "): blindagem desativada.";
         }
+        return "NaveCargueira (ID#" + id + "): não pode desativar blindagem em movimento.";
+    }
 
-        return relatorioCarga;
+    public String controlarManual() {
+        return "NaveCargueira (ID#" + id + "): controle manual ativado.";
+    }
+
+    public String carregar(int peso) {
+        cargaAtual += peso;
+        String msg = "Carga atual: " + cargaAtual;
+        if ((double) cargaAtual / capacidadeMaximaCarga > 0.7)
+            msg += ". " + ativarBlindagem();
+        return "NaveCargueira (ID#" + id + "): " + msg;
     }
 
     public String descarregar(int peso) {
-        this.cargaAtual = this.cargaAtual - peso;
-        if (this.cargaAtual < 0) {
-            this.cargaAtual = 0; // Evita que a carga fique com valor negativo
-        }
-        return "NaveCargueira (ID#" + this.id + "): Carga descarregada. Carga atual é " + this.cargaAtual;
-    }
-
-    @Override
-    public String decolar() {
-        return "NaveCargueira (ID#" + this.id + "): decolando.";
+        cargaAtual -= peso;
+        if (cargaAtual < 0) cargaAtual = 0;
+        return "NaveCargueira (ID#" + id + "): carga descarregada. Atual: " + cargaAtual;
     }
 }
